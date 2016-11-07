06f5046d9397f8f3b9225cd6f0fd6bbd8366ffd2
Thomas Scheibe <th.scheibe@litixsoft.de>
2016-11-07 13:09:02 +0100

    fix: build error on unix systems

--- browsers/Edge.js
+++ browsers/Edge.js
@@ -1,9 +1,16 @@
 var resolve = require('resolve');
+var CMD;
+
+try {
+    CMD = resolve.sync('edge-launcher/Win32/MicrosoftEdgeLauncher.exe');
+} catch (e) {
+    return;
+}
 
 module.exports = {
     name: 'Edge',
     DEFAULT_CMD: {
-        win32: [resolve.sync('edge-launcher/Win32/MicrosoftEdgeLauncher.exe')]
+        win32: [CMD]
     },
     ENV_CMD: 'EDGE_BIN'
 };
