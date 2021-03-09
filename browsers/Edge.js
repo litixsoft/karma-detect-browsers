1058fb7bcccf6f3b8ce1995210b25a6b4d0f7db5
Aleen <aleen42@vip.qq.com>
2021-03-09 16:02:25 +0800

    Detect Chromium Edge with the plugin @chiragrupani/karma-chromium-edge-launcher

--- browsers/Edge.js
+++ browsers/Edge.js
@@ -1,15 +1,20 @@
-var CMD;
+var linux, darwin, win32;
 
 try {
-    CMD = require.resolve('edge-launcher/dist/x86/MicrosoftEdgeLauncher.exe');
-} catch (e) {
-    CMD = '';
-}
+    var CMD = require('@chiragrupani/karma-chromium-edge-launcher')['launcher:Edge'][1].prototype.DEFAULT_CMD;
+    linux = CMD.linux, darwin = CMD.darwin, win32 = CMD.win32;
+} catch (ignore) {}
+
+try {
+    win32 = require.resolve('edge-launcher/dist/x86/MicrosoftEdgeLauncher.exe');
+} catch (ignore) {}
 
 module.exports = {
     name: 'Edge',
     DEFAULT_CMD: {
-        win32: [CMD]
+        linux: [linux || ''],
+        darwin: [darwin || ''],
+        win32: [win32 || '']
     },
     ENV_CMD: 'EDGE_BIN'
 };
